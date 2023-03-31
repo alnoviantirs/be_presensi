a96ce5e62d304ed67711f0787b2665f8648c479d
Novi <alnoviantiramadhani91@gmail.com>
2023-03-31 15:27:22 +0700

    Tugas6-Al Novianti Ramadhani

--- js/fetch.js
+++ js/fetch.js
@@ -1,6 +1,13 @@
-import { get } from "https://bukulapak.github.io/api/process.js"; 
+import { get } from "https://bukulapak.github.io/api/process.js";
+import { setInner } from "https://bukulapak.github.io/element/process.js";
 let urlAPI = "https://alnovianti.herokuapp.com/presensi";
 get(urlAPI,isiTablePresensi);
 function isiTablePresensi(results){
     console.log(results);
+    results.forEach(isiRow);
 }
+function isiRow(value){
+    console.log(value)
+}
+
+
