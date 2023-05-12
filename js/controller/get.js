96f5df40bd40f6e4b90d3d1d87e5e941df5bfda6
Novi <alnoviantiramadhani91@gmail.com>
2023-05-12 16:23:52 +0700

    latihan9

--- js/controller/get.js
+++ js/controller/get.js
@@ -1,21 +1,47 @@
 import { addInner } from "https://bukulapak.github.io/element/process.js";
-import { getRandomColor, getRandomColorName } from "https://bukulapak.github.io/image/process.js";
+import {
+  getRandomColor,
+  getRandomColorName,
+} from "https://bukulapak.github.io/image/process.js";
 import { isiTabel } from "../temp/table.js";
 export function isiTablePresensi(results) {
-    results.forEach(isiRow);
+  results.forEach(isiRow);
 }
 function isiRow(value) {
-    let content =
-        isiTabel.replace("#NAMA#", value.biodata.nama)
-            .replace("#NOHP#", value.biodata.phone_number)
-            .replace("#JABATAN#", value.biodata.jabatan)
-            .replace("#LOKASI#", value.location)
-            .replace("#STATUS#", value.checkin)
-            .replace("#HARIKERJA#", value.biodata.hari_kerja ? value.biodata.hari_kerja: "HARIKERJA")
-            .replace("#JAMKERJA#", value.biodata.hari_kerja ? value.biodata.jam_kerja [0].durasi : "JAMKERJA")
-            .replace("#JAMMASUK#", value.biodata.hari_kerja ? value.biodata.jam_kerja [0].jammasuk : "JAMMASUK")
-            .replace("#JAMKELUAR#", value.biodata.hari_kerja ?value.biodata.jam_kerja [0].jamkeluar : "JAMKELUAR")
-            .replace("#WARNA#", getRandomColor())
-            .replace(/#WARNALOGO#/g, getRandomColorName());
-    addInner("iniTabel", content);
+  let content = isiTabel
+    .replace("#NAMA#", value.biodata.nama)
+    .replace(
+      "#NOHP#",
+      value.biodata.phone_number
+        ? value.biodata.phone_number
+        : value.phone_number
+        ? value.phone_number
+        : "#NOHP#"
+    )
+    .replace("#JABATAN#", value.biodata.jabatan)
+    .replace("#LOKASI#", value.location)
+    .replace("#STATUS#", value.checkin)
+    .replace(
+      "#HARIKERJA#",
+      value.biodata.hari_kerja ? value.biodata.hari_kerja : "HARIKERJA"
+    )
+    .replace(
+      "#JAMKERJA#",
+      value.biodata.hari_kerja ? value.biodata.jam_kerja[0].durasi : "JAMKERJA"
+    )
+    .replace(
+      "#JAMMASUK#",
+      value.biodata.hari_kerja
+        ? value.biodata.jam_kerja[0].jammasuk
+        : "JAMMASUK"
+    )
+    .replace(
+      "#JAMKELUAR#",
+      value.biodata.hari_kerja
+        ? value.biodata.jam_kerja[0].jamkeluar
+        : "JAMKELUAR"
+    )
+    .replace("#WARNA#", getRandomColor())
+    .replace(/#WARNALOGO#/g, getRandomColorName());
+  addInner("iniTabel", content);
 }
