4fd07037e508e50b11b4e5eff59484ae99436e07
Novi <alnoviantiramadhani91@gmail.com>
2023-05-19 16:13:37 +0700

    up

--- js/config/url_get_detail.js
+++ js/config/url_get_detail.js
@@ -1,4 +1,4 @@
 const urlParams = new URLSearchParams(window.location.search);
 const presensiId = urlParams.get("presensiId");
 
-export let urlFetch = "https://alnovianti.herokuapp.com/presensi" + presensiId;
+export let urlFetch = "https://alnovianti.herokuapp.com/presensi/" + presensiId;
