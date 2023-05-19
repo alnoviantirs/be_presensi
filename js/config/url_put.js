5adeff6fb07506279394adceb875f80738430665
Novi <alnoviantiramadhani91@gmail.com>
2023-05-19 15:30:50 +0700

    latihan10

---
+++ js/config/url_put.js
@@ -0,0 +1,10 @@
+const urlParams = new URLSearchParams(window.location.search);
+const presensiId = urlParams.get("presensiId");
+
+export let urlPUT = "https://alnovianti.herokuapp.com/upd/" + presensiId;
+
+export function AmbilResponse(result) {
+  console.log(result); //menampilkan respond API pada console
+  alert(result); //menampilkan respond API pada alert
+  window.location.href = "index.html"; //reload halaman setelah klik ok pada alert
+}
